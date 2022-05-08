f89545536597331fad1146fcbe7813cbbb1cb58a
James Strocel <info@james-strocel.com>
2022-05-07 19:28:07 -0700

    Switching to pg

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@user1 = User.create(email: "anybody@home.com", password: "password", name: Faker::Name.name, about_me: Faker::Marketing.buzzwords )
-@user2 = User.create(email: "everybody@here.com", password: "password", name: Faker::Name.name, about_me: Faker::Marketing.buzzwords )
+@user1 = User.create!(email: "anybody@home.com", password: "password", name: Faker::Name.name, about_me: Faker::Marketing.buzzwords )
+@user2 = User.create!(email: "everybody@here.com", password: "password", name: Faker::Name.name, about_me: Faker::Marketing.buzzwords )
 User.all.each do |user|
     10.times do 
-      user.posts.create(title: Faker::Book.title, body: Faker::Lorem.paragraph)    
+      user.posts.create!(title: Faker::Book.title, body: Faker::Lorem.paragraph)    
     end
 end
 
 Post.all.each do |post|
-    @user1.comments.create(post:post, body: 'I am making a comment')
-    @user2.comments.create(post:post, body: 'I am  responding to your comment')
+    @user1.comments.create!(post:post, body: 'I am making a comment')
+    @user2.comments.create!(post:post, body: 'I am  responding to your comment')
 end
 
 
